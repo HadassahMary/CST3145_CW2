f2cbd62f223af7c75ff211c1ce73ff1515431e43
HadassahMary <hadassahmj01@gmail.com>
2022-02-12 18:10:51 +0400

    changed errors

--- server.js
+++ server.js
@@ -46,8 +46,7 @@ app.post('/collection/:collectionName', (req, res, next) => {
 });
 
 const ObjectID = require('mongodb').ObjectID;
-app.get('/collection/:collectionName/:id'
-, (req, res, next) => {
+app.get('/collection/:collectionName/:id', (req, res, next) => {
 req.collection.findOne({ _id: new ObjectID(req.params.id) }, (e, result) => {
 if (e) return next(e)
 res.send(result)
