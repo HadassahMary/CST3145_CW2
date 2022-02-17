4214f3ad6327cf25a22f0771da42004e97e996c6
HadassahMary <hadassahmj01@gmail.com>
2022-02-17 16:16:07 +0400

    commenting

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//adding all the required modules.
 const { request } = require('express');
 const express = require('express');
 var path = require("path");
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.set('port', 3000)
 
+//logger middleware. Logs all the incoming requests
 app.use(function(req, res, next) {
     console.log("In comes a " + req.method + " request to " + req.url);
     next();
@@ -21,9 +23,11 @@ app.use((req, res, next) => {
 
 let db;
 
+//connection of the mongoclient
 MongoClient.connect('mongodb+srv://Hadassah:Hadassah2001@cluster0.exxms.mongodb.net', (err, client) => {
     db = client.db('schoolclub');
 });
+
 
 app.get('/', (req, res, next) => {
     res.send('Select a collection, e.g., /collectionName');
@@ -34,6 +38,7 @@ app.param('collectionName', (req, res, next, collectionName) => {
     return next();
 });
 
+//GET request to view collections from MongoDB
 app.get('/collection/:collectionName', (req, res, next) => {
     req.collection.find({}).toArray((e, results) => {
         if (e) return next(e)
@@ -41,6 +46,7 @@ app.get('/collection/:collectionName', (req, res, next) => {
     });
 });
 
+//GET request with a search query
 app.get('/collection/:collectionName/:k', (req, res) => {
     var key_1 = req.params.k;
     console.log("Searched term: " + key_1);
@@ -51,6 +57,7 @@ app.get('/collection/:collectionName/:k', (req, res) => {
     });
 });
 
+//POST request to add the order into the database
 app.post('/collection/:collectionName', (req, res, next) => {
     req.collection.insertOne(req.body, (e, results) => {
         if (e) return next(e);
@@ -59,6 +66,8 @@ app.post('/collection/:collectionName', (req, res, next) => {
 });
 
 const ObjectID = require('mongodb').ObjectID;
+
+//GET request to find a collection with a given ID
 app.get('/collection/:collectionName/:id', (req, res, next) => {
 req.collection.findOne({ _id: new ObjectID(req.params.id) }, (e, result) => {
 if (e) return next(e)
@@ -66,6 +75,7 @@ res.send(result)
 })
 })
 
+//PUT request to update the spaces of the lessons
 app.put('/collection/:collectionName/:id', (req, res, next) => {
     req.collection.update(
     {_id: new ObjectID(req.params.id)},
@@ -73,17 +83,14 @@ app.put('/collection/:collectionName/:id', (req, res, next) => {
     {safe: true, multi: false},
     (e, result) => {
     if (e) return next(e)
-    // res.send((result.result.n === 1) ? {msg: 'success'} : {msg: 'error'})
     res.send(
-    
         result.modifiedCount === 1 ? { msg: "success" } : { msg: "error" }
-        
         );
     
     })
     })
 
-
+//Static middleware. This middleware checks if an image exists in the static folder
     app.use(function(req, res, next){
         var filePath = path.join(__dirname, "static", req.url);
         fs.stat(filePath, function(err, fileInfo){
@@ -99,11 +106,14 @@ app.put('/collection/:collectionName/:id', (req, res, next) => {
             }
         });
     });
+
+//If an image does not exist in the static folder, the error message is displayed.
     app.use(function(req, res){
         res.status(404);
         res.send("File not Found! try again...");
     });   
 
+//deploying the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log("Server running...");
